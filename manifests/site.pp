d4fc30822bce910c206f45b3bc7f5c27e559dab3
jblackburn311 <jeremy.blackburn.dp7j@statefarm.com>
2016-05-24 14:24:20 -0700

    uncomment nginx

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,6 +43,7 @@ node 'jblackburn311.puppetlabs.vm' {
   include users     #including users module
   include skeleton
   include memcached
+  #include nginx
   
   exec { 'motd cowsay':
       path    => '/usr/local/bin',
