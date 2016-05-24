28011f151d379332c7a8218b1aec344532c13786
jblackburn311 <jeremy.blackburn.dp7j@statefarm.com>
2016-05-24 11:26:17 -0700

    bashrc file

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,3 +1,14 @@
-file { '/etc/skel/.bashrc:
-  ensure => file
+class skeleton {
+  file { '/etc/skel' :
+    ensure => directory,
+  }
+  
+  file { '/etc/skel/.bashrc' :
+    ensure => file,
+    group => 'root',
+    owner =>
+    mode => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
+  }
+}
   
